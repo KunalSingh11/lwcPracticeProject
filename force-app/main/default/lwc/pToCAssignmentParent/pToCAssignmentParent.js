d2e849e30e37e89323ea8e20aad58f02df22150f
Kunal Singh <kunal070598@gmail.com>
2024-05-05 00:28:52 +0530

    Initial commit

---
+++ force-app/main/default/lwc/pToCAssignmentParent/pToCAssignmentParent.js
@@ -0,0 +1,24 @@
+import { LightningElement, track, wire } from 'lwc';
+import getRecentAccounts from '@salesforce/apex/AccountController.getRecentAccounts';
+export default class PToCAssignmentParent extends LightningElement {
+
+    @track selectedAccountId;
+    @track options = [];
+
+    @wire(getRecentAccounts)
+    wiredRecentAccounts({ error, data }) {
+        if (data) {
+            this.options = data.map(account => ({
+                label: account.Name,
+                value: account.Id
+            }));
+        } else if (error) {
+            console.error('Error retrieving recent accounts', error);
+        }
+    }
+
+    handleRadioChange(event) {
+        this.selectedAccountId = event.detail.value;
+    }
+
+}
